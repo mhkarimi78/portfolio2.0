31535ef951f4a618ab47c00533bad4eaba34b690
mhkarimi78 <mahsa_karim16@yahoo.com>
2023-04-21 12:25:19 +0330

    added contact me

---
+++ components/ContactMe.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitErrorHandler } from "react-hook-form";
+
+type Props = {};
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
+
+function ContactMe({}: Props) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+  const onSubmit: SubmitErrorHandler<Inputs> = (data) => console.log(data);
+  return (
+    <div
+      className="h-screen flex relative flex-col text-center md:text-left 
+    md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+    >
+      <h3 className=" absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+        Contact
+      </h3>
+      <div className=" flex flex-col space-y-10">
+        <h4 className=" text-4xl font-semibold text-center">
+          I have got just what you need.
+          <span className=" decoration-[#F7AB0A]/50 underline">Lets Talk.</span>
+        </h4>
+        <div className="space-y-10">
+          <div className="flex items-center space-x-5 justify-center">
+            <PhoneIcon className="h-7 w-7 animate-pulse text-[#F7AB0A]" />
+            <p className="text-2xl">+989101124003</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <EnvelopeIcon className="h-7 w-7 animate-pulse text-[#F7AB0A]" />
+            <p className="text-2xl">mahs.karimi16@gmail.com</p>
+          </div>
+          <div className="flex items-center space-x-5 justify-center">
+            <MapPinIcon className="h-7 w-7 animate-pulse text-[#F7AB0A]" />
+            <p className="text-2xl">Pasdaran, Tehran, Iran</p>
+          </div>
+        </div>
+        <form
+          className="flex flex-col space-y-2 w-fit mx-auto"
+          // onSubmit={handleSubmit(onSubmit)}
+        >
+          <div className="flex space-x-2">
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="email"
+            />
+          </div>
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          />
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default ContactMe;
